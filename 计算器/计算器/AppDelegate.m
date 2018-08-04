4c7600de3908494f8910c50526ba1d55ce69b3a2
chenjiao <18001651153@163.com>
2018-08-04 17:57:06 +0800

    创建了home

--- 计算器/计算器/AppDelegate.m
+++ 计算器/计算器/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //appdelegate做了修改
     return YES;
 }
 
---
+++ 计算器/计算器/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  计算器
+//
+//  Created by chenjiao on 2018/8/4.
+//  Copyright © 2018年 yaoyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
